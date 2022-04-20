a3725e97bd1b724d25a53f9049ffd6585c01f680
MisaelFloresB <misaelf82@gmail.com>
2022-04-20 15:08:06 -0500

    this.dateCreate y this.lastUpdate cambiaron su valor a new Date() en el archivo user.js, se utilizo not.toBeUndefined en el archivo User.test.js para dateCreated y lastUpdate

--- twitter/test/models/User.test.js
+++ twitter/test/models/User.test.js
@@ -2,14 +2,16 @@ const User = require('./../../app/models/user')
 
 describe("Unit test for User class", () =>{
     test('Create an User object', () =>{
-        const user = new User(1, "MisaelF", "Misael Flores", "Bio", "dateCreated", "lastUpdate")
+        const user = new User(1, "MisaelF", "Misael Flores", "Bio")
 
         expect(user.id).toBe(1)
         expect(user.userName).toBe("MisaelF")
         expect(user.name).toBe("Misael Flores")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdate).toBe("lastUpdate")
+        // expect(user.dateCreated).toBe("dateCreated")
+        // expect(user.lastUpdate).toBe("lastUpdate")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdate).not.toBeUndefined()
     });
 
 })
