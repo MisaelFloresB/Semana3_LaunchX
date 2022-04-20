414b967ae334a3cc684f0796d29d36c018af6892
MisaelFloresB <misaelf82@gmail.com>
2022-04-20 12:54:56 -0500

    Se agrega la carpeta app, test y el archivo spiderman.test.js tambien el archivo spiderman.js

---
+++ spiderverse/app/spiderman.js
@@ -0,0 +1,17 @@
+class spiderman{
+    constructor(name, age, actor, movies, cinemaStudio){
+        this.name = name,
+        this.age = age,
+        this.actor = actor,
+        this.movies = movies,
+        this.cinemaStudio = cinemaStudio
+    }
+    // name = "Spiderman"
+    // age = "23"
+    // actor = "Tom Holland"
+    // movies = "3"
+    // cinemaStudio = "Marvel/Sony Studios"
+
+}
+
+module.exports = spiderman
