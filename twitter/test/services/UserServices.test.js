5eeefa1947bedfb205caac94a5b8029b9a972615
MisaelFloresB <misaelf82@gmail.com>
2022-04-21 13:41:06 -0500

    Se agrego archivo de pruebas para UserServices

---
+++ twitter/test/services/UserServices.test.js
@@ -0,0 +1,22 @@
+const UserService = require('./../../app/services/UserService')
+
+describe("Test for UserService", () =>{
+    test("1, Create a new user using the UserService",() =>{
+
+        const user = UserService.create(1, "MisaelFlores", "Misael")
+        expect(user.userName).toBe("MisaelFlores")
+        expect(user.name).toBe("Misael")
+        expect(user.id).toBe(1)
+        expect(user.bio).not.toBeUndefined()
+    });
+
+    test("1, Get all user data in a list",() =>{
+
+        const user = UserService.create(1, "MisaelFlores", "Misael")
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe(1)
+        expect(userInfoList[1]).toBe("MisaelFlores")
+        expect(userInfoList[2]).toBe("Misael")
+        expect(userInfoList[3]).toBe("Sin Bio")
+    })
+});
