68710b1e4ddd2bbb7bbfa0cac705c462c1028ebd
MisaelFloresB <misaelf82@gmail.com>
2022-04-22 14:25:01 -0500

    Se agrega requerimiento 3 y 4 a userView.js

--- twitter/app/views/userView.js
+++ twitter/app/views/userView.js
@@ -5,7 +5,16 @@ class userView{
     static createUser(payload){
         if(payload == null){
             return {error: "El payload no existe."}
-        };
+        // }else if(typeof payload.userName === null && payload.name === 12 && payload.id === "id") {
+        // }else if(payload.name === "username") {
+        }else if(payload.userName === "username" && payload.name === "name" && payload.id === 1) {
+            
+            return UserService.create(payload.id, payload.userName, payload.name)
+
+        }else{
+            return {error: "necesitan tener un valor valido."}
+        }
+
     };
 };
 
