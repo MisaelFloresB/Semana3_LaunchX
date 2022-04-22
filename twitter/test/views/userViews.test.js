c447cb764b04a4b54ef227cf8523d3a83fc1d081
MisaelFloresB <misaelf82@gmail.com>
2022-04-22 14:26:13 -0500

    Se agrega test para requerimiento 3 y 4

--- twitter/test/views/userViews.test.js
+++ twitter/test/views/userViews.test.js
@@ -7,4 +7,27 @@ describe("Tests for user view", ()=>{
         expect(result.error).toMatch(/payload no existe/)
 
     });
+
+    test("Return an error object when try to create a new user with invalid properties", () => {
+        const payload = {userName: null, name:12, id:"id"}
+        const result = userView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+
+    });
+
+    test("Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {userName: "username"}
+        const result = userView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+
+    });
+
+    test("Create a user by a given valid payload", () => {
+        const payload = {userName: "username", id: 1, name: "name"}
+        const result = userView.createUser(payload)
+        expect(result.name).toBe("name")
+        expect(result.userName).toBe("username")
+        expect(result.id).toBe(1)
+
+    });
 });
